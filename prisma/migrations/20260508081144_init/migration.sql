760486ad4c5feb524ba604f55328add893cd2b2a
turab577 <abuturabhassankhan@gmail.com>
2026-05-08 13:17:58 +0500

    refactor: migrate database from SQLite to PostgreSQL
    - Updated package dependencies to use PostgreSQL adapter for Prisma.
    - Removed old SQLite migration files and created new migration for PostgreSQL.
    - Adjusted schema.prisma to reflect the change in database provider.

---
+++ prisma/migrations/20260508081144_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "otp" TEXT,
+    "otpExpiry" TIMESTAMP(3),
+    "image" TEXT,
+    "twoFactorSecret" TEXT,
+    "twoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
